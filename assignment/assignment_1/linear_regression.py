ab4e23e837ac9d1e20568010c29f56ecf8f08eef
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2020-04-02 22:00:02 -0700

    assignment 1 working

---
+++ assignment/assignment_1/linear_regression.py
@@ -0,0 +1,14 @@
+# CS 434 - Spring 2020
+# Team members - Group 33
+# Junhyeok Jeong, jeongju@oregonstate.edu
+# Youngjoo Lee, leey3@oregonstate.edu
+
+import math
+import sys
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+        # read csv file with pandas library (put file path exactly)
+        housing_test = pd.read_csv('C:\Users\wnsgu\Desktop\CS-434\assignment\assignment_1\housing_test.csv') 
+        housing_train = pd.read_csv('C:\Users\wnsgu\Desktop\CS-434\assignment\assignment_1\housing_train.csv')
