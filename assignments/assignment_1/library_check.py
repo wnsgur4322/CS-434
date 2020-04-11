9e3b63806961b90f8fe96eda2952b5863d154c7d
epww1502 <43550999+epww1502@users.noreply.github.com>
2020-04-11 13:48:31 -0700

    Update library_check.py

--- assignments/assignment_1/library_check.py
+++ assignments/assignment_1/library_check.py
@@ -5,9 +5,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib as plt
+import sklearn as sk
 
 if __name__ == "__main__":
 	#print out library version on the terminal
 	print("numpy version: %s" % np.__version__)
 	print("pandas version: %s" % pd.__version__)
 	print("matplotlib version: %s" % plt.__version__)
+	print("sklearn version: %s" % sk.__version__)
+
