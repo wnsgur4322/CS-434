57f43805e64842d68f3209d4a4b6cc9d2872dbb2
epww1502 <43550999+epww1502@users.noreply.github.com>
2020-04-10 17:08:59 -0700

    Update q2_1.py

--- assignments/assignment_1/q2_1.py
+++ assignments/assignment_1/q2_1.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import argparse as arg
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import math
 
 # command line prompt with argparse library
 parser = arg.ArgumentParser()
@@ -28,16 +29,41 @@ def logistic_function(w, x):
     return 1.0/(1 + np.exp(-np.dot(x, w.T))) #(700,1) or (400, 1)
 
 def logistic_gradient(w, x, y):
-    temp = (1.0/(1 + np.exp(-np.dot(x, w.T)))) - y.reshape(x.shape[0], -1)
+    #print(y.reshape(x.shape[0], -1))
+    temp = logistic_function(w, x) - y
     result = np.dot(temp.T, x)
     return result
+
+'''
+# sigmoid function (L(w) - for q2)
+def logistic_function(w, x):
+    
+    row, col = x.shape
+    result = np.zeros((row, col))
+    #print(result.shape)
+    for i in range(0,row-1):
+        result[i] = 1.0/(1 + np.exp(-np.dot(x[i], w.T)))
+
+    print(result)
+    return result
+
+def logistic_gradient(w, x, y):
+    row, col = x.shape
+    yhat = np.zeros((row,1))
+    gradient = np.zeros((row,1))
+    for i in range(0, row-1):
+        yhat = 1.0/(1 + np.exp(-np.dot(x[i], w.T)))
+        gradient = gradient + np.dot((yhat - y[i]),x[i])
+    print(gradient)
+    return gradient
+'''
 
 def learning_w(w, x, y):
     logistic_fun = logistic_function(w, x)
     y = np.squeeze(y)
 
     result = -(y*np.log(logistic_fun)) - ((1-y)*np.log(1-logistic_fun))
-    return np.mean(result)
+    return result
 
 def batch_grad_desc(x, y, w, learning_r, eps, x_type, y_type):
 
@@ -46,12 +72,36 @@ def batch_grad_desc(x, y, w, learning_r, eps, x_type, y_type):
     accuracy = []
 
     condition = True
-    cost = learning_w(w, x, y)
+    #cost = learning_w(w, x, y)
     check = 1
+    row, col = w.shape
 
+    gradient = np.zeros((row,col))
 
     while condition:
+        row, col = w.shape
+        gradient = np.zeros((row,col))
+        #temp = cost
+        gradient = logistic_gradient(w, x, y)
+        w = w - (learning_r*gradient)
+        #cost = learning_w(w, x, y)
+        #check = temp - cost
+        temp_accuracy = prediction(w, x_type, y_type)
+        index.append(iteration)
+        accuracy.append(temp_accuracy)
+        #print(gradient*gradient.T, math.sqrt(gradient*gradient.T))
+        #if(math.sqrt(np.sum(gradient**2, axis=1))) <= eps:
+        if (math.sqrt(gradient*gradient.T)) <= eps:
+            condition = False
+        elif iteration >= 500:
+            condition = False
+        iteration += 1
+    
+    '''
+    while condition:
+        gradient = 0
         temp = cost
+        gradient = (0.01*logistic_gradient(w, x, y))
         w = w - (0.01*logistic_gradient(w, x, y))
         cost = learning_w(w, x, y)
         check = temp - cost
@@ -63,15 +113,17 @@ def batch_grad_desc(x, y, w, learning_r, eps, x_type, y_type):
         elif iteration >= 500:
             condition = False
         iteration += 1
-
+    '''
     return w, iteration, index, accuracy
+
+
 
 
 def prediction(w, x_type, y_type):
     pred_prob = logistic_function(w, x_type)
     pred_value = np.where(pred_prob >= .5, 1, 0)
     result = np.matrix(np.squeeze(pred_value)).T
-    return np.sum(y_type == result)/np.size(result) * 100
+    return np.sum(y_type == result)/np.size(y_type) * 100
 
 
 if __name__ == '__main__':
@@ -96,9 +148,10 @@ if __name__ == '__main__':
     w_train = np.matrix(np.zeros(train_x_train.shape[1]))
     w_test = np.matrix(np.zeros(test_x_train.shape[1]))
 
-    w, iteration, index, accuracy = batch_grad_desc(train_x_train, train_y_train, w_train, learningrate, 0.1, train_x_test, train_y_test)
+    w, iteration, index, accuracy = batch_grad_desc(train_x_train, train_y_train, w_train, learningrate, 0.001, train_x_test, train_y_test)
 
-    print("total accuracy of training(%): ", accuracy[499])
+    m = len(accuracy)
+    print("Accuracy of training(%): ", accuracy[m-1], " / Size of iteration: ", m)
     plt.plot(index,accuracy)
     plt.title("Plot of the learning curve (Training accuracy)")
     plt.xticks(np.arange(0, 500, step = 100), rotation='vertical')
@@ -108,12 +161,11 @@ if __name__ == '__main__':
 
     w_test, iteration, index, accuracy = batch_grad_desc(test_x_train, test_y_train, w_test, learningrate, 0.001, test_x_test, test_y_test)
     
-
-    print("total accuracy of test(%): ", accuracy[499])
+    m = len(accuracy)
+    print("Accuracy of test(%): ", accuracy[m-1], " / Size of iteration: ", m)
     plt.plot(index,accuracy)
     plt.title("Plot of the learning curve (Test accuracy)")
     plt.xticks(np.arange(0, 500, step = 100), rotation='vertical')
     plt.xlabel("Number of Iterarion")
     plt.ylabel("Accuracy (%)")
     plt.show()
-
