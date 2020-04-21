5e1052077ffe0d4dfbdec0bd9ff687f77166c363
Junhyeok Jeong <jeongju@vm-cs434-1.engr.oregonstate.edu>
2020-04-21 01:22:47 -0700

    working

--- assignments/assignment_2/starter.py
+++ assignments/assignment_2/starter.py
@@ -1,12 +1,12 @@
+# CS 434 - Spring 2020
+# Team members - Group 33
+# Junhyeok Jeong, jeongju@oregonstate.edu
+# Youngjoo Lee, leey3@oregonstate.edu
+
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 import re
-
-# Importing the dataset
-imdb_data = pd.read_csv('IMDB.csv', delimiter=',')
-
-
 
 def clean_text(text):
 
@@ -30,21 +30,6 @@ def clean_text(text):
     text = text.translate(translate_map)
 
     return text
-
-
-# this vectorizer will skip stop words
-vectorizer = CountVectorizer(
-    stop_words="english",
-    preprocessor=clean_text
-)
-
-# fit the vectorizer on the text
-vectorizer.fit(imdb_data['review'])
-
-# get the vocabulary
-inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
-vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
-#print(vocabulary)
 
 def create_bow(input):
     '''
@@ -72,19 +57,47 @@ def create_bow(input):
 
     return bag_of_words, name
 
-bow, name = create_bow(vocabulary)
-bow = np.sum(bow, axis=0)
-print(bow.shape)
-#print(name)
-model = pd.DataFrame( 
-    (count, word) for word, count in
-    zip(bow.T, name))
-model.columns = ['Word', 'Count']
-#model.sort_values('Count', ascending=False, inplace=True)
-model.head()
-print(model)
-#model['voca'] = name
-#model['count'] = bow[1]
+if __name__ == "__main__":
+    # Importing the dataset
+    imdb_data = pd.read_csv('IMDB.csv', delimiter=',')
 
-#model.sort_values(by=['count'], ascending=False, inplace=True)
-#print(model.head(10))
+    # divide the dataset into train, test, and validation
+    imdb_data = imdb_data.to_numpy()
+    imdb_data = imdb_data.tolist()
+
+    print(imdb_data[0][0])
+    
+    # this vectorizer will skip stop words
+    vectorizer = CountVectorizer(
+        stop_words="english",
+        preprocessor=clean_text
+    )
+
+    # fit the vectorizer on the text
+    vectorizer.fit(imdb_data[0])
+    print(len(imdb_data[0]))
+
+
+    # get the vocabulary
+    inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
+    vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
+    print(vocabulary)
+'''
+    bow, name = create_bow(vocabulary)
+    bow = np.sum(bow, axis=0)
+    print(bow.shape)
+    #print(name)
+    model = pd.DataFrame( 
+        (count, word) for word, count in
+        zip(bow.T, name))
+    model.columns = ['Word', 'Count']
+    #model.sort_values('Count', ascending=False, inplace=True)
+    model.head()
+    print(model)
+    #model['voca'] = name
+    #model['count'] = bow[1]
+
+    #model.sort_values(by=['count'], ascending=False, inplace=True)
+    #print(model.head(10))
+
+'''
