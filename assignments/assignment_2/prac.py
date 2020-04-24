55185dd36e93ceb4bd65f02c6c02fd146130a31e
Junhyeok Jeong <jeongju@vm-cs434-1.engr.oregonstate.edu>
2020-04-24 11:46:11 -0700

    working

--- assignments/assignment_2/prac.py
+++ assignments/assignment_2/prac.py
@@ -9,8 +9,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 import re
 from collections import Counter
 import math
-from matplotlib import pyplot as plt
-import csv
 
 def clean_text(text):
 
@@ -23,7 +21,6 @@ def clean_text(text):
     text = re.sub(r"\"", "", text)
     #pattern = r'[^a-zA-z0-9\s]'
     #text = re.sub(pattern, '', text)
-    text = re.sub(r'\d+', "", text)
 
     # convert text to lowercase
     text = text.strip().lower()
@@ -41,11 +38,12 @@ def create_bow(input, vocabulary):
         analyzer = "word",
         tokenizer=None,
         preprocessor=clean_text,
-        stop_words=None,
-        vocabulary=vocabulary,
+        stop_words="english",
+        #vocabulary=vocabulary,
         max_features=2000
     )
     bag_of_words = vectorizer.fit_transform(input)
+    #np.asarray(bag_of_words)
     bag_of_words = bag_of_words.toarray()
     
     name = vectorizer.get_feature_names()
@@ -67,77 +65,82 @@ def word_prob(model_type, reviews, word, index):
 
 # To calculate the total number of words in reviews (positive or negative)
 def total_num(reviews, pos_neg):
-    res = 0
+    res = []
     for i in range(len(reviews)):
-        review = list(map(str, clean_text(reviews[i]).split(" ")))       
+        review = list(map(str, clean_text(reviews[i]).split(" ")))
         review = list(filter(('').__ne__, review))
-        res += len(review)
+        
+        res += review
+    
+    
+    
+    #print(res)
 
-    print("The total number of words in all %s reviews: %d" % (pos_neg,res))
-    return res
+    print("The total number of words in all %s reviews: %d" % (pos_neg,len(res)))
+    return len(res)
 
-# apply the multi-nomial Naive Bayes classifier with Laplace smooth (Q2: a = 1, Q4:a = 0 ... 2)
-def conditional_probability(model_type, total_num, index, pos_neg, alpha):
-    # formula: (the number of words in class(pos or neg) + Laplace smooth (1)) / (&total number of words in class + &bag of words size (2000))
-    res = float((model_type['Count'][index] + alpha) / (total_num + 2000))
+# apply the multi-nomial Naive Bayes classifier with Laplace smooth (a = 1)
+def conditional_probability(model_type, total, index, pos_neg):
+    # formula: (the number of words in class(pos or neg) + Laplace smooth (1)) / (total number of words in class + bag of words size (2000))
+    res = (model_type['Count'][index] + 1) / (total + 2000)
+    print("P(%dth word | %s) : %f" % (index + 1, pos_neg, res))
     return res
-    #CP fomular: ((# of words appearances in pos or neg) + 1) / (total # of words in pos (duplication is counted)) + 2000)
 
 from functools import reduce
 # Question 3 part
 def multiply(arr):
-    return reduce(lambda x, y: x * y, arr)
+    res = 0
+    for i in range(len(arr)):
+        res = res + arr[i]
+    #res = reduce(lambda x, y: x + y, arr)
+    #print(res)
+    return res
 
 def MNB_valid(sentence, pos_CP, neg_CP, train_pos_prob, train_neg_prob):
+        #res = []
         word_count = []
-
+        
         for j in range(len(train_model['Word'])):
+            #count = 0
+            
+            '''
+            for h in range(len(sentence)):
+                if sentence[h] == valid_model['Word'][j]:
+                    count += 1
+
+            '''
+            #word_count.append(count)
             word_count.append(sentence.count(train_model['Word'][j]))
 
-        pos_pow_list = [math.log(wi) * n for wi, n in zip(pos_CP, word_count)]
+        print(len(word_count))
+
+
+        pos_pow_list = [ n * math.log(wi) for wi, n in zip(pos_CP, word_count)]
         pos_pow_list = list(filter((0.0).__ne__, pos_pow_list))
-        
 
-        neg_pow_list = [math.log(wi) * n for wi, n in zip(neg_CP, word_count)]
+        neg_pow_list = [ n * math.log(wi) for wi, n in zip(neg_CP, word_count)]
         neg_pow_list = list(filter((0.0).__ne__, neg_pow_list))
-        
         # P(Positive | Validation reviews) = train_pos_prob * pos_CP[word_1]^n * pos_CP[word_2] ....
-        pos_res = math.log(train_pos_prob) + reduce(lambda x, y: x + y, pos_pow_list)
         
+        #pos_res = math.log(train_pos_prob) + reduce(lambda x, y: x + y, pos_pow_list)
+        pos_res = math.log(train_pos_prob) +  multiply(pos_pow_list)
+        #pos_res = multiply(pos_pow_list)
+        print(pos_res)
         # P(Negative | Validation reviews) = train_neg_prob * neg_CP[word_1]^n * neg_CP[word_2] ....
-        neg_res = math.log(train_neg_prob) + reduce(lambda x, y: x + y, neg_pow_list)
-
+        #neg_res = math.log(train_neg_prob) + reduce(lambda x, y: x + y, neg_pow_list)
+        neg_res = math.log(train_neg_prob) + multiply(neg_pow_list)
+        #neg_res = multiply(neg_pow_list)
+        print(neg_res)
+        
 
         if pos_res > neg_res:
+            print("positive")
             return("positive")
+            #res.append("positive")
         else:
-            return("negative")     
-
-def prediction(sentence, pos_CP, neg_CP, train_pos_prob, train_neg_prob):
-        word_count = []
-
-        for j in range(len(train_model['Word'])):
-            word_count.append(sentence.count(train_model['Word'][j]))
-
-        pos_pow_list = [math.log(wi) * n for wi, n in zip(pos_CP, word_count)]
-        pos_pow_list = list(filter((0.0).__ne__, pos_pow_list))
-        
-
-        neg_pow_list = [math.log(wi) * n for wi, n in zip(neg_CP, word_count)]
-        neg_pow_list = list(filter((0.0).__ne__, neg_pow_list))
-        
-        # P(Positive | Validation reviews) = train_pos_prob * pos_CP[word_1]^n * pos_CP[word_2] ....
-        pos_res = math.log(train_pos_prob) + reduce(lambda x, y: x + y, pos_pow_list)
-        
-        # P(Negative | Validation reviews) = train_neg_prob * neg_CP[word_1]^n * neg_CP[word_2] ....
-        neg_res = math.log(train_neg_prob) + reduce(lambda x, y: x + y, neg_pow_list)
-
-        if pos_res > neg_res:
-            # positive
-            return("1")
-        else:
-            # negative
-            return("0")     
+            print("negative")
+            return("negative")
+            #res.append("negative")
 
 if __name__ == "__main__":
     # Importing the dataset
@@ -163,18 +166,20 @@ if __name__ == "__main__":
     for i in range(0, num_texts):
         cleaned_text.append(clean_text(imdb_data['review'][i]))
 
-    bow, name = create_bow(imdb_data['review'], vocabulary)
+    bow, name = create_bow(cleaned_text, vocabulary)
     bow = np.sum(bow, axis=0)
+    print(bow[0])
 
     model = pd.DataFrame( 
     (count, word) for word, count in
     zip(bow, name))
     model.columns = ['Word', 'Count']
+    #model.sort_values('Count', ascending=False, inplace=True)
     print(model)
     print("done ... !")
 
     print("generating BOW for 30k (training set) reviews ...")
-    bow_train, name_train = create_bow(imdb_data['review'][:30000], vocabulary)
+    bow_train, name_train = create_bow(cleaned_text[:30000], vocabulary)
     bow_train = np.sum(bow_train, axis=0)
 
     train_model = pd.DataFrame( 
@@ -187,7 +192,9 @@ if __name__ == "__main__":
 
     # divide the dataset into train, test, and validation
     train_set = list(imdb_data['review'][:30000])
+    #train_set = imdb_data.iloc[:30000]
     valid_set = list(imdb_data['review'][30000:40000])
+    #valid_set = imdb_data.iloc[30000:40000]
     test_set = list(imdb_data['review'][40000:])
     
     print("the lengths of three data set: train_set = %d, valid_set = %d, test_set = %d" % (len(train_set), len(valid_set), len(test_set)))
@@ -215,55 +222,30 @@ if __name__ == "__main__":
     # Conditional Probabilities: P(Wi...2000|Positive) & P(Wi...2000|Negative)
     # formula: (the number of words in class(pos or neg) + Laplace smooth (1)) / (total number of words in class + bag of words size (2000))
     # P(Wi...2000|Positive) part
-    
-    posbow_train, posname_train = create_bow(train_pos, vocabulary)
-    posbow_train = np.sum(posbow_train, axis=0)
-
-    postrain_model = pd.DataFrame( 
-    (count, word) for word, count in
-    zip(posbow_train, posname_train))
-    postrain_model.columns = ['Word', 'Count']
-    print(postrain_model)
-    print(len(postrain_model))
-    
-    negbow_train, negname_train = create_bow(train_neg, vocabulary)
-    negbow_train = np.sum(negbow_train, axis=0)
-
-    negtrain_model = pd.DataFrame( 
-    (count, word) for word, count in
-    zip(negbow_train, negname_train))
-    negtrain_model.columns = ['Word', 'Count']
-    print(negtrain_model)
-    print(len(negtrain_model))
-
     pos_CP = []
     total_pos = total_num(train_pos, "Positive")
     for i in range(2000):
-        pos_CP.append(conditional_probability(postrain_model, total_pos, i, "Positive", 1))
-
+        pos_CP.append(conditional_probability(train_model, total_pos, i, "Positive"))
+    
     #P(Wi...2000|Negative) Part
     neg_CP = []
     total_neg = total_num(train_neg, "Negative")
     for i in range(2000):
-        neg_CP.append(conditional_probability(negtrain_model, total_neg, i, "Negative", 1))
-    '''
-    for i in range(2000):
-        print("test ", i, ": ", pos_CP[i] - neg_CP[i])
-    '''
-
+        neg_CP.append(conditional_probability(train_model, total_neg, i, "Negative"))
 
     # 3-1. Apply the learned Naive Bayes model to the validation set
     # P(Positive | Validation review 1) = train_pos_prob * pos_CP[word_1]^n * pos_CP[word_2] ....
+
     print("generating BOW for 10k (validation set) reviews ...")
-    bow_valid, name_valid = create_bow(imdb_data['review'][30000:40000], vocabulary)
+    bow_valid, name_valid = create_bow(cleaned_text[30000:40000], vocabulary)
     bow_valid = np.sum(bow_valid, axis=0)
 
     valid_model = pd.DataFrame( 
     (count, word) for word, count in
     zip(bow_valid, name_valid))
     valid_model.columns = ['Word', 'Count']
-    print("valid model:")
     print(valid_model)
+    print(len(valid_model))
     print("done ... !")
 
     sentences = []
@@ -272,63 +254,21 @@ if __name__ == "__main__":
         sentence = valid_set[i].split(" ")
         sentence = list(filter(('').__ne__, sentence))
         sentences.append(sentence)
-    print("sentence separation is done .. !")        
-    '''
+        print(i)
+    
+    print("done .. !")        
+    
+    #print(valid_label)
+
+    #sys.end(1)
     acc = 0
     validation_res = []
     for i in range(len(valid_set)):
         validation_res.append(MNB_valid(sentences[i], pos_CP, neg_CP, train_pos_prob, train_neg_prob))
+        print(i)
+    #print(validation_res)
 
     for j in range(len(validation_res)):
         if validation_res[j] == valid_label[j]:
             acc += 1
-    print("validation accuracy: %f" % float(acc/100))
-    '''
-    # 3-2. Apply the learned Naive Bayes model to the test set and predict
-    print("generating BOW for 10k (test set) reviews ...")
-    bow_test, name_test = create_bow(imdb_data['review'][40000:], vocabulary)
-    bow_test = np.sum(bow_test, axis=0)
-
-    test_model = pd.DataFrame( 
-    (count, word) for word, count in
-    zip(bow_test, name_test))
-    test_model.columns = ['Word', 'Count']
-    print("test model:")
-    print(test_model)
-    print("done ... !")
-
-    tsentences = []
-    for i in range(len(test_set)):
-        test_set[i] = clean_text(test_set[i])
-        sentence = test_set[i].split(" ")
-        sentence = list(filter(('').__ne__, sentence))
-        tsentences.append(sentence)
-    print("done .. !")
-                
-    test_predictions = []
-
-    for i in range(len(test_set)):
-        test_predictions.append(prediction(tsentences[i], pos_CP, neg_CP, train_pos_prob, train_neg_prob))
-        print("test looping ... %d" % i)
-
-    print("Prediction result for test(10k)")
-    print(test_predictions)
-    print("done ... !")
-
-    # save the result of testing data prediction on test-prediction1.csv
-    field = ['sentiment']
-    rows = test_predictions
-    
-    with open("test-prediction1.csv", 'w') as csvfile:
-        write_csv = csv.writer(csvfile)
-        # put field
-        write_csv.writerow(field)
-        # put rows
-        write_csv.writerows(rows)
-    print("test-prediction1.csv file is successfully created !")
-
-    # 4. Tuning smoothing parameter alpha. Train the Naive Bayes classifier with different values of α between 0 to 2 (incrementing by 0.2).
-    a = 0
-    
-
-    
+    print("validation accuracy: %f" % (float(acc/100)))            
