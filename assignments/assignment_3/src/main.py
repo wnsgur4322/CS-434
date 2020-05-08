b9f2eb4224aa80b4f4330c7a6839b9febb5e1556
Junhyeok Jeong <jeongju@vm-cs434-1.engr.oregonstate.edu>
2020-05-08 00:00:48 -0700

    Q1 complete

--- assignments/assignment_3/src/main.py
+++ assignments/assignment_3/src/main.py
@@ -33,7 +33,8 @@ def county_info(args):
 	dictionary_info(county_dict)
 
 def decision_tree_testing(x_train, y_train, x_test, y_test, max_depth):
-	print('Decision Tree\n')
+	print('Decision Tree')
+	print("depth : %d" % max_depth)
 	
 	clf = DecisionTreeClassifier(max_depth)
 	clf.fit(x_train, y_train)
@@ -47,7 +48,7 @@ def decision_tree_testing(x_train, y_train, x_test, y_test, max_depth):
 	preds_train =clf.predict(x_train)
 
 	print('F1 Train {}'.format(f1(y_train, preds_train)))
-	print('F1 Test {}'.format(f1(y_test, preds)))
+	print('F1 Test {}\n'.format(f1(y_test, preds)))
 	
 	return train_accuracy, test_accuracy, f1(y_train, preds_train), f1(y_test, preds) 
 	
@@ -84,7 +85,6 @@ if __name__ == '__main__':
 		f1_test_accs = []
 
 		for i in range(25):
-			print("depth : %d" % (i + 1))
 			train_acc, test_acc, f1_train_acc, f1_test_acc = decision_tree_testing(x_train, y_train, x_test, y_test, i + 1)
 			train_accs.append(train_acc * 100)
 			test_accs.append(test_acc * 100)
@@ -94,6 +94,16 @@ if __name__ == '__main__':
 		# Q1 - D plot part
 		tree_draw_plot_1(train_accs, test_accs)
 		tree_draw_plot_2(f1_train_accs, f1_test_accs)
+		
+		# Q1 - E part
+		print("The best accuracies (train, test, F1_train, F1_test): %f %f %f %f" % (max(train_accs), max(test_accs), max(f1_train_accs), max(f1_test_accs)))
+		with open("accuracy_result.txt", "w") as res:
+			res.write("train_acc\ttest_acc\tF1_train_acc\tF1_test_acc\n")
+			for i in range(len(train_accs)):
+				res.write("{0}\t{1}\t{2}\t{3}\n".format(train_accs[i], test_accs[i], f1_train_accs[i], f1_train_accs[i]))
+			
+			res.close()
+		print("accuracy_result.txt created !\n")
 
 	if args.random_forest == 1:
 		random_forest_testing(x_train, y_train, x_test, y_test)
