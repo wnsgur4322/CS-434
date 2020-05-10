c3a007f4cd65317496e76d463e367b38e19593d6
epww1502 <43550999+epww1502@users.noreply.github.com>
2020-05-10 00:33:51 -0700

    Update tree.py

--- assignments/assignment_3/src/tree.py
+++ assignments/assignment_3/src/tree.py
@@ -256,178 +256,6 @@ class RandomForestClassifier():
 # MUST MODIFY THIS EXISTING DECISION TREE CODE #
 ################################################
 
-'''
-class DecisionTreeAdaBoost():
-	def __init__(self, max_depth=None):
-		self.max_depth = max_depth
-
-	# take in features X and labels y
-	# build a tree
-	def fit(self, X, y, feature_idx=None, weights=None):
-		self.num_classes = len(set(y))
-		self.class_labels = list(set(y))
-		if feature_idx is None:
-			self.features_idx = np.arange(0, X.shape[1])
-		else:
-			self.features_idx = feature_idx
-
-		if weights is None:
-			weights = np.ones(X.shape[0])
-		self.root = self.build_tree(X, y, 1, np.asarray(weights))
-
-	# make prediction for each example of features X
-	def predict(self, X):
-		preds = [self._predict(example) for example in X]
-
-		return preds
-
-	# prediction for a given example
-	# traverse tree by following splits at nodes
-	def _predict(self, example):
-		node = self.root
-		while node.left_tree:
-			if example[node.feature] < node.split:
-				node = node.left_tree
-			else:
-				node = node.right_tree
-		return node.prediction
-
-	# accuracy
-	def accuracy_score(self, X, y):
-		preds = self.predict(X)
-		accuracy = (preds == y).sum()/len(y)
-		return accuracy
-
-	# function to build a decision tree
-	def build_tree(self, X, y, depth, weights):
-
-		# store data and information about best split
-		# used when building subtrees recursively
-		best_feature = None
-		best_split = None
-		best_gain = 0.0
-		best_left_X = None
-		best_left_y = None
-		best_right_X = None
-		best_right_y = None
-		best_left_weight = None
-		best_right_weight = None
-
-		# what we would predict at this node if we had to
-		# majority class
-		num_samples_per_class = [np.sum(y == i) for i in self.class_labels]
-		prediction = self.class_labels[np.argmax(num_samples_per_class)]
-
-		# if we haven't hit the maximum depth, keep building
-		if depth <= self.max_depth:
-			# consider each feature
-			for feature in self.features_idx:
-				# consider the set of all values for that feature to split on
-				possible_splits = np.unique(X[:, feature])
-				for split in possible_splits:
-					# get the gain and the data on each side of the split
-					# >= split goes on right, < goes on left
-					gain, left_X, right_X, left_y, right_y, left_weights, right_weights = self.check_split(X, y, feature, split, weights)
-					# if we have a better gain, use this split and keep track of data
-					if gain > best_gain:
-						best_gain = gain
-						best_feature = feature
-						best_split = split
-						best_left_X = left_X
-						best_right_X = right_X
-						best_left_y = left_y
-						best_right_y = right_y
-						best_left_weight = left_weights
-						best_right_weight = right_weights
-		
-		# if we haven't hit a leaf node
-		# add subtrees recursively
-		if best_gain > 0.0:
-			left_tree = self.build_tree(best_left_X, best_left_y, depth+1, best_left_weight)
-			right_tree = self.build_tree(best_right_X, best_right_y, depth+1, best_right_weight)
-			print("get in", depth)
-			return Node(prediction=prediction, feature=best_feature, split=best_split, left_tree=left_tree, right_tree=right_tree)
-
-		# if we did hit a leaf node
-		return Node(prediction=prediction, feature=best_feature, split=best_split, left_tree=None, right_tree=None)
-
-
-	# gets data corresponding to a split by using numpy indexing
-	def check_split(self, X, y, feature, split, weights):
-		left_idx = np.where(X[:, feature] < split)
-		right_idx = np.where(X[:, feature] >= split)
-
-		left_X = X[left_idx]
-		right_X = X[right_idx]
-		left_y = y[left_idx]
-		right_y = y[right_idx]
-		left_weights = weights[left_idx]
-		right_weights = weights[right_idx]
-
-		# calculate gini impurity and gain for y, left_y, right_y
-		gain = self.calculate_gini_gain(y, left_y, right_y, weights, left_weights, right_weights)
-		return gain, left_X, right_X, left_y, right_y, left_weights, right_weights
-
-	def calculate_gini_gain(self, y, left_y, right_y, weights, left_weights, right_weights):
-		# not a leaf node
-		# calculate gini impurity and gain
-		gain = 0
-		if len(left_y) > 0 and len(right_y) > 0:
-			x = np.sum((y == self.class_labels[0]) * weights)
-			cp = np.sum((y == self.class_labels[0]) * weights)
-			cn = np.sum((y == self.class_labels[1]) * weights)
-			clp = np.sum((left_y == self.class_labels[0]) * left_weights)
-			cln = np.sum((left_y == self.class_labels[1]) * left_weights)
-			crp = np.sum((right_y == self.class_labels[0]) * right_weights)
-			crn = np.sum((right_y == self.class_labels[1]) * right_weights)
-			ul = 1 - pow(clp / (clp + cln), 2) - pow(cln / (clp + cln), 2)
-			ur = 1 - pow(crp / (crp + crn), 2) - pow(crn / (crp + crn), 2)
-			ua = 1 - pow(cp / (cp + cn), 2) - pow(cn / (cp + cn), 2)
-			pl = (clp + cln) / (cp + cn)
-			pr = (crp + crn) / (cp + cn)
-			gain = ua - (pl * ul) - (pr * ur)
-			return gain
-		# we hit leaf node
-		# don't have any gain, and don't want to divide by 0
-		else:
-			return 0
-
-class AdaBoostClassifier:
-	def __init__(self, num_trees, max_depth):
-		self.num_trees = num_trees
-		self.max_depth = max_depth
-		self.trees = []
-		self.alphas = []
-
-	def train(self, X, y):
-		# Initialize D1(i) = 1/N for all i from 1 to N
-		weights = []
-		self.trees = []
-		self.alphas = []
-		weights.append(np.ones(X.shape[0]) / X.shape[0])
-
-		for i in range(0, self.num_trees):
-			# create new tree
-			newTree = DecisionTreeAdaBoost(max_depth=self.max_depth)
-			newTree.fit(X, y, weights=weights[i])
-			self.trees.append(newTree)
-
-			# Calc error rate
-			error = 1 - newTree.accuracy_score(X, y)
-			alpha = .5 * math.log((1-error)/error, math.e)
-			self.alphas.append(alpha)
-			predict = newTree.predict(X)
-			# Calc new weight factor
-			new_weight = weights[i] * np.exp(alpha * ((predict != y) * 2 - 1))
-			normalize_factor = sum(new_weight)
-			weights.append( new_weight / normalize_factor)
-
-	def predict(self, X):
-		y = np.zeros(X.shape[0])
-		for i in range(0, len(self.trees)):
-			y = y + np.asarray(self.trees[i].predict(X)) * self.alphas[i]
-		return np.sign(y)
-'''
 class DecisionTreeAdaBoost():
 	def __init__(self):
 		self.max_depth = 1 # The depth is 1 = Decision Stump.
@@ -546,24 +374,6 @@ class DecisionTreeAdaBoost():
 		err = self.calculate_weighted_err(y, left_y, right_y, left_w, right_w, weights)
 		return err, left_X, right_X, left_y, right_y, left_w, right_w
 
-	'''
-	def calculate_weighted_err(self, y, left_y, right_y, left_w, right_w):
-		# not a leaf node
-		# calculate gini impurity and gain
-		err = 0
-		if len(left_y) > 0 and len(right_y) > 0:
-			l_neg = np.count_nonzero(left_y == -1)
-			r_pos = np.count_nonzero(right_y == 1)
-
-			err = (l_neg+r_pos)/len(y)
-
-			print("err: ", err)
-			return err
-		# we hit leaf node
-		# don't have any gain, and don't want to divide by 0
-		else:
-			return 0
-	'''
 	def calculate_weighted_err(self, y, left_y, right_y, left_w, right_w, w):
 		# not a leaf node
 		# calculate gini impurity and gain
@@ -573,49 +383,19 @@ class DecisionTreeAdaBoost():
 		r_product = [a*b for a,b in zip(right_y,right_w)]
 		t_product = [a*b for a,b in zip(y,w)]
 
-		'''
-		if len(left_y) > 0 and len(right_y) > 0:
-			# For U(AL) part
-			l_pos = 0.0
-			l_neg = 0.0
-			for i in len(left_y):
-				if(left_y[i] == 1):
-					l_pos += left_w[i]
-				else:
-					l_neg += left_w[i]
-		'''	
 		if len(left_y) > 0 and len(right_y) > 0:
 			l_pos = sum(filter(lambda a: a>=0, l_product))
 			l_neg = abs(sum(filter(lambda a: a<0, l_product)))
 			l_pos_p = l_pos / (l_pos+l_neg)
 			l_neg_p = l_neg / (l_pos+l_neg)
 
-			
-			'''
-			# For U(AR) part
-			r_pos = 0.0
-			r_neg = 0.0
-			for i in len(right_y):
-				if(right_y[i] == 1):
-					r_pos += right_w[i]
-				else:
-					r_neg += right_w[i]
-			'''
+
 			r_pos = sum(filter(lambda a: a>=0, r_product))
 			r_neg = abs(sum(filter(lambda a: a<0, r_product)))
 			r_pos_p = r_pos / (r_pos+r_neg)
 			r_neg_p = r_neg / (r_pos+r_neg)
 
 			# For U(A) part
-			'''
-			t_pos = 0.0
-			t_neg = 0.0
-			for i in len(y):
-				if(y[i] == 1):
-					t_pos += w[i]
-				else:
-					t_neg += w[i]
-			'''
 			t_pos = sum(filter(lambda a: a>=0, t_product))
 			t_neg = abs(sum(filter(lambda a: a<0, t_product)))
 			t_pos_p = t_pos / (t_pos+t_neg)
@@ -631,7 +411,6 @@ class DecisionTreeAdaBoost():
 			# B = U(A) − plU(AL) − prU(AR)
 			err = u_top - ((l_pos+l_neg)*u_left) - ((r_pos+r_neg)*u_right)
 
-			print("err: ", err)
 			return err
 		# we hit leaf node
 		# don't have any gain, and don't want to divide by 0
@@ -649,9 +428,9 @@ class AdaBoostClassifier():
 		stumps = []
 
 		#initialize weights
-		evaluations = pd.DataFrame(y.copy())
+		#evaluations = pd.DataFrame(y.copy())
 		# set all weights as 1/n (initial weights)
-		evaluations['weights'] = 1/len(y)
+		#evaluations['weights'] = 1/len(y)
 		weights = []
 		alphas = []
 		weights.append(np.ones(X.shape[0]) / X.shape[0])
@@ -660,15 +439,29 @@ class AdaBoostClassifier():
 			stump = DecisionTreeAdaBoost()
 			stump.fit(X, y, weights = weights[i])
 
-			print("done")
-			print("Stump: ", stump)
 			stumps.append(stump)
-			print("err")
-			predictions = stump.predict(X)
-			print(predictions)
-			print("err")
-			evaluations['predictions'] = predictions
-			evaluations['evaluation'] = np.where(evaluations['predictions'] == evaluations['target'], 1, 0)
+			prediction = stump.predict(X)
+
+			classified = np.where(prediction == y, 1, 0)
+			missclassified = np.where(prediction != y, 1, 0)
+
+			accuracy = sum(classified)/len(classified)
+			miss = sum(missclassified)/len(missclassified)
+
+			err = np.sum(weights*missclassified)/np.sum(weights)
+
+			alpha = (1/2)*np.log((1-err)/err)
+			print("ALPHA: ", alpha)
+			alphas.append(alpha)
+			print(weights)
+			weights *= np.exp(alpha*missclassified)
+			
+			print(weights)
+			'''
+			print("acc", accuracy)
+			exit(1)
+			evaluations['predictions'] = prediction
+			evaluation = np.where(predictions == y, 1, 0)
 			evaluations['misclassified'] = np.where(evaluations['predictions'] != evaluations['target'],1,0)
 
 			accuracy = sum(evaluations['evaluation'])/len(evaluations['evaluation'])
@@ -683,7 +476,7 @@ class AdaBoostClassifier():
 
 
 			evaluations['weights'] *= np.exp(alpha*evaluations['misclassified'])
-
+			'''
 		self.alphas = alphas
 		self.stumps = stumps
 
