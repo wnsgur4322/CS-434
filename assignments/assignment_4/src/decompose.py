77e9184c0d46ebe53f58c43925546a09ef58c753
epww1502 <43550999+epww1502@users.noreply.github.com>
2020-05-30 14:14:36 -0700

    Update decompose.py

--- assignments/assignment_4/src/decompose.py
+++ assignments/assignment_4/src/decompose.py
@@ -95,11 +95,14 @@ class PCA():
             if s >= target:
                 index = i
                 break
+            if (i == len(self.eig_vals)-1):
+                index = i+1
                
-        print("Index: ", i)
+        print("Index: ", index)
         self.eig_vals = self.eig_vals[:index]
         self.eig_vecs = self.eig_vecs[:, :index]
-
+        print("eig_value.shape: ", self.eig_vals.shape, " | eig_vector.shape: ", self.eig_vecs.shape)
+        
 
 
     def transform(self, x):
